feb73fda1908d0cd29b429fca9e25ee45792bf2c
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-12-01 14:32:27 +0200

    Added routes, adjust container and added display: flex

---
+++ src/Components/ScheduledCalls/ScheduledCalls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function ScheduledCalls() {
+    return (
+        <div class='ScheduledCalls'>
+            <h1>Scheduled Calls</h1>
+        </div>
+    );
+}
+
+export default ScheduledCalls
