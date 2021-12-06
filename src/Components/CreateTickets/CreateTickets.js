3d8e907c9c083d274f435454bc0ea4e7c846eb0f
buivis <gustasbui@gmail.com>
2021-12-06 15:24:36 +0200

    added solution,software,problem text areas

--- src/Components/CreateTickets/CreateTickets.js
+++ src/Components/CreateTickets/CreateTickets.js
@@ -3,11 +3,13 @@ import '../../App.css';
 import './CreateTickets.css'
 import SearchBar from '../SearchBar/SearchBar';
 import TextBoxes from '../SearchBar/TextBoxes';
+import Forms from '../SearchBar/Forms';
 function CreateTicket() {
     return (
         <div class='CreateTickets'>
             <SearchBar placeholder="Enter customer search string" />
             <TextBoxes />
+            <Forms />
         </div>
     );
 }
