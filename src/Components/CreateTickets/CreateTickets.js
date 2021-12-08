421418870c05e6d532c7b40c27df53f7b4193591
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-12-08 22:22:31 +0200

    Trying to implement firebase

--- src/Components/CreateTickets/CreateTickets.js
+++ src/Components/CreateTickets/CreateTickets.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import app from '../../firebase';
+import React, {useState} from 'react';
 import '../../App.css';
 import './CreateTickets.css'
 import SearchBar from '../SearchBar/SearchBar';
 import TextBoxes from '../SearchBar/TextBoxes';
 import Forms from '../SearchBar/Forms';
+import { getDatabase, ref, set } from "firebase/database";
+
 function CreateTicket() {
+
+    const [name , setName] = useState();
+    const [age , setAge] = useState();
+
+    function writeTicket() {
+        const db = getDatabase();
+        set(ref(db, 'users/' ), {
+          name: name,
+          age: age,
+        });
+      }
+
     return (
         <div class='CreateTickets'>
             <SearchBar placeholder="Enter customer search string" />
+            <form onSubmit={writeTicket}>
             <TextBoxes />
             <Forms />
+            <button type='submit'>Submit</button>
+            </form>
         </div>
     );
 }
